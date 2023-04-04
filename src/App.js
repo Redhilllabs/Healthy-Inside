9df990a96d364fd8bcf570fb35a8fb8546d5288d
ketanu0690 <78308610+ketanu0690@users.noreply.github.com>
2023-04-04 15:56:26 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,74 +1,67 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import { useState, useEffect, useCallback } from "react";
+import { getAllFoodItems } from "./utils/ApiCall";
+import { actionType } from "./context/reducer";
+import { useStateValue } from "./context/StateProvider";
+import PrivateRoutes from "./Routes/PrivateRoute";
+import PublicRoutes from "./Routes/PublicRoute";
+import AdminRoutes from "./Routes/AdminRoute";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
 import Signup from "./components/Signup";
 import Morninigfood from "./components/Morninigfood";
-import Footer from "./components/Footer";
 import Login from "./components/Login";
 import AdminLogin from "./components/AdminLogin";
 import Account from "./components/Account";
 import OrderSubmit from "./components/OrderSubmit";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Cart from "./components/Cart";
-import { getAllFoodItems } from "./utils/ApiCall";
-import React, { useEffect, useState } from "react";
-import { useStateValue } from "./context/StateProvider";
-import { actionType } from "./context/reducer";
-import PrivateRoutes from "./Routes/PrivateRoute";
-import PublicRoutes from "./Routes/PublicRoute";
-import AdminRoutes from "./Routes/AdminRoute"
+import './App.css'
 
 function App() {
   const [showProfile, setShowProfile] = useState(false);
   const [{ foodItems }, dispatch] = useStateValue();
 
-  const handleProfileToggle = () => {
-    setShowProfile(!showProfile);
-  };
+  const handleProfileToggle = useCallback(() => {
+    setShowProfile(prev => !prev);
+  }, []);
 
-  // const fetchData = async () => {
-  //   await getAllFoodItems().then((data) => {
-  //     dispatch({
-  //       type: actionType.SET_FOOD_ITEMS,
-  //       foodItems: data.data,
-  //     });
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getAllFoodItems();
+      dispatch({ type: actionType.SET_FOOD_ITEMS, payload: data });
+    }
+    fetchData();
+  }, [dispatch]);
 
   return (
-    <div className="App">
+    <>
       <Router>
-      <Header onProfileToggle={handleProfileToggle} />
+        <Header onProfileToggle={handleProfileToggle} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/morninigfood" element={<Morninigfood />} />
 
           <Route element={<AdminRoutes></AdminRoutes>}>
-          {/* <Route path="/urvi" element={<Dashboard  showProfile={showProfile} onProfileToggle={handleProfileToggle} />}></Route> */}
+            <Route path="/urvi" element={<Dashboard showProfile={showProfile} />} />
           </Route>
 
           <Route element={<PrivateRoutes></PrivateRoutes>}>
-          <Route path="/account" element={<Account />}></Route>
-          <Route path="/cart" element={<Cart />}></Route>
-          <Route path="/oderSubmit" element={<OrderSubmit />}></Route>
-          
+            <Route path="/account" element={<Account />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/oderSubmit" element={<OrderSubmit />} />
           </Route>
 
           <Route element={<PublicRoutes></PublicRoutes>}>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/Adminlogin" element={<AdminLogin />}></Route>
-            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/Adminlogin" element={<AdminLogin />} />
+            <Route path="/signup" element={<Signup />} />
           </Route>
-
         </Routes>
         <Footer />
       </Router>
-    </div>
+    </>
   );
 }
 
