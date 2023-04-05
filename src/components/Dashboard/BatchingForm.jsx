a62af18b05781f47c69ddc14b8aa7bbb99ced2e1
ketanu0690 <78308610+ketanu0690@users.noreply.github.com>
2023-04-05 15:00:46 +0530

    new change

--- src/components/Dashboard/BatchingForm.jsx
+++ src/components/Dashboard/BatchingForm.jsx
@@ -3,7 +3,6 @@ import { SearchBatchingImportAndexport } from "../../utils/ApiCall";
 function BatchingForm({ date }) {
   const [data, setData] = useState([]);
 
-
   useEffect(()=>{
 if(date){
   fetchData()
@@ -24,8 +23,10 @@ const response = await SearchBatchingImportAndexport(bodyContent)
 alert("date not present ")
       }
       else{
-        setData(response.data);
-      }
+      setData(response.data);
+    }
+
+
 
     } catch (error) {
       console.log(error);
@@ -37,67 +38,97 @@ alert("date not present ")
       <h2>Batching Form</h2>
 
       {date && (
+        <div id="Tabels_container" >
         <div className="table-container">
-  <h2> Kitchen</h2>
-  <br />
-  <table className="recipe_table">
-    <thead>
-      <tr>
-        <th>Root Item</th>
-        <th>Import supply</th>
-        <th>Export supply</th>
-        <th>Headed For</th>
+  <table className="recipe_table" id='yourrecipetale'>
+  <thead>
+    <tr>
+      <th>Root Item</th>
+      <th>Import supply</th>
+      <th>Export supply</th>
+      <th>Headed For</th>
+    </tr>
+  </thead>
+  <tbody>
+    {data.map((item) => (
+      <tr key={item.id}>
+        <td>{item.rootItem}</td>
+        <td>
+          <table>
+            <thead>
+              <tr>
+                <th>Particulars</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {item.importSupply.map((supply, index) => (
+                <tr key={index}>
+                  <td>{supply.particulars}</td>
+                  <td>{supply.quantity}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </td>
+        <td>
+          <table>
+            <thead>
+              <tr>
+                <th>Particulars</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {item.exportSupply.map((supply, index) => (
+                <tr key={index}>
+                  <td>{supply.particulars}</td>
+                  <td>{supply.quantity}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </td>
+        <td>{item.headedFor}</td>
       </tr>
-    </thead>
-    <tbody>
-      {data.map((item) => (
-        <tr key={item.id}>
-          <td>{item.rootItem}</td>
-          <td>
-            <table>
-              <thead>
-                <tr>
-                  <th>Particulars</th>
-                  <th>Quantity</th>
-                </tr>
-              </thead>
-              <tbody>
-                {item.importSupply.map((supply, index) => (
-                  <tr key={index}>
-                      <>
-                        <td>{supply.particulars}</td>
-                        <td>{supply.quantity}</td>
-                      </>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </td>
-          <td>
-            <table>
-              <thead>
-                <tr>
-                  <th>Particulars</th>
-                  <th>Quantity</th>
-                </tr>
-              </thead>
-              <tbody>
-                {item.exportSupply.map((supply, index) => (
-                  <tr key={index}>
-                      <>
-                        <td>{supply.particulars}</td>
-                        <td>{supply.quantity}</td>
-                      </>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </td>
-          <td>{item.headedFor}</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
+    ))}
+  </tbody>
+</table>
+
+</div>
+
+
+<div className="table-container" id="showtablermi" >
+<table className="recipe_table">
+  <thead>
+    <tr>
+      <th>Particulars</th>
+      <th>Quantity</th>
+    </tr>
+  </thead>
+  <tbody>
+  {data.reduce((accumulator, item) => {
+  item.importSupply.forEach((supply) => {
+    const index = accumulator.findIndex((obj) => obj.particulars === supply.particulars);
+    if (index !== -1) {
+      accumulator[index].quantity += supply.quantity;
+    } else {
+      accumulator.push(supply);
+    }
+  });
+  return accumulator;
+}, []).map((supply, index) => (
+  <tr key={index}>
+    <td>{supply.particulars}</td>
+    <td>{supply.quantity}</td>
+  </tr>
+))}
+  </tbody>
+</table>
+
+
+</div>
+
 </div>
 
       )}
